b95f1b4d5f4f723c4f43426d373d69bf6bd99b0e
Queenshiba <haruka0111.2a@gmail.com>
2019-08-25 14:10:31 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// alert("hey");
+
+// window.onscroll = function() {myFunction};
+
+// var header = document.getElementById("myHeader");
+// var sticky = header.offsetTop;
+
+// function myFunction() {
+//     if (window.pageYOffset > sticky){
+//         header.classList.add("sticky");
+//         sticky.style.top = `-${sr2.height + 1}px`;
+//     } else {
+//         header.classList.remove("sticky");
+//         sticky.style.top = 0;
+//     }
+}
